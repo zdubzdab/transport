cf3da4a3568793a8d6b0ad73251d3cb7f0cf2353
zdubzdab <zdubzdab@gmail.com>
2016-10-26 11:38:56 +0300

    Set time in accordance to Belgium

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -8,7 +8,7 @@ var Clock = React.createClass({
     var day = date.getDate();
     var year = date.getFullYear();
     var weekday = new Array(7);
-    weekday[0]=  "Sunday";
+    weekday[0] = "Sunday";
     weekday[1] = "Monday";
     weekday[2] = "Tuesday";
     weekday[3] = "Wednesday";
@@ -18,7 +18,7 @@ var Clock = React.createClass({
     var dayName = weekday[date.getDay()];
 
     var month = new Array(12);
-    month[0]=  "January";
+    month[0] = "January";
     month[1] = "February";
     month[2] = "March";
     month[3] = "April";
@@ -41,27 +41,31 @@ var Clock = React.createClass({
   },
 
   setTime: function(){
-    var currentdate = new Date();
+    //transform date to Belgium
+    Date.prototype.minusHour = function(h){
+        this.setHours(this.getHours()-h);
+        return this;
+    }
+
+    var currentdate = new Date().minusHour(1);
     var hours = currentdate.getHours();    
+    // add leading zero, first convert hours to string
+    hours = hours + "";
+    if( hours.length == 1 ){ hours = "0" + hours; }
 
-      // add leading zero, first convert hours to string
-      hours = hours + "";
-      if( hours.length == 1 ){ hours = "0" + hours; }
+    var minutes = currentdate.getMinutes();
+    // add leading zero, first convert hours to string
+    minutes = minutes + "";
+    if( minutes.length == 1 ){ minutes = "0" + minutes; }
 
-      var minutes = currentdate.getMinutes();
-      // add leading zero, first convert hours to string
-      minutes = minutes + "";
-      if( minutes.length == 1 ){ minutes = "0" + minutes; }
-
-      var seconds = currentdate.getSeconds();
-      seconds = seconds + "";
-      if( seconds.length == 1 ){ seconds = "0" + seconds; }
-
-      this.setState({
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds
-      });
+    var seconds = currentdate.getSeconds();
+    seconds = seconds + "";
+    if( seconds.length == 1 ){ seconds = "0" + seconds; }
+    this.setState({
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    });
   },
   componentWillMount: function(){
     this.setTime();
@@ -73,7 +77,6 @@ var Clock = React.createClass({
     }.bind(this), 1000);
   },
   render: function() {
-    
     return(
       <div className="city-row">
         <span className="city-time">{this.state.hours}:{this.state.minutes}:{this.state.seconds}</span>
