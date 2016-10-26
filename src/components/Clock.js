5003da6cb23a1a8f9877f4318da46db6dc9ed8f0
zdubzdab <zdubzdab@gmail.com>
2016-10-26 11:43:40 +0300

    Refactor setDate function

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -7,30 +7,12 @@ var Clock = React.createClass({
     var date = new Date();
     var day = date.getDate();
     var year = date.getFullYear();
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     var dayName = weekday[date.getDay()];
 
-    var month = new Array(12);
-    month[0] = "January";
-    month[1] = "February";
-    month[2] = "March";
-    month[3] = "April";
-    month[4] = "May";
-    month[5] = "June";
-    month[6] = "July";
-    month[7] = "August";
-    month[8] = "September";
-    month[9] = "October";
-    month[10] = "November";
-    month[11] = "December";
-    var monthName = month[date.getMonth()];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August",
+                  "September", "October", "November", "December"]
+    var monthName = months[date.getMonth()];
 
     this.setState({
       dayName: dayName,
